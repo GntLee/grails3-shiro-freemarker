6a260fcb57bcf78cc1b8e5aeeb8c647a02d9c4c4
GntLee <30571119+GntLee@users.noreply.github.com>
2018-05-06 17:38:31 +0800

    Update resources.groovy
    更新防止hibernate session报错配置

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,6 +3,9 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver
 // Place your Spring DSL code here
 beans = {
 
+    //防止hibernate session报错
+    openSessionInViewFilter(org.springframework.orm.hibernate4.support.OpenSessionInViewFilter) {}
+    
     freeMarkerConfigurer(com.system.FreeMarkerConfigExtend) {
         templateLoaderPath = "\\"
     }
